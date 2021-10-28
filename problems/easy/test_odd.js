7aae9b387d2a179847aba454917c544aaf0d3ad1
Azeb.K <54040527+azebkidanemariam@users.noreply.github.com>
2021-10-28 11:38:16 +0200

    function s update on test file

--- problems/easy/test_odd.js
+++ problems/easy/test_odd.js
@@ -6,7 +6,7 @@ const xs = [1, 2, 3, 4, 5]
 describe('Array', function () {
  describe("#odd", function ()  {
    it("[1, 2, 3, 4, 5]", function () { 
-    assert.deepEqual(odd(xs)([1, 2, 3, 4, 5]), [1, 3, 5])
+    assert.deepEqual(odd([1, 2, 3, 4, 5]), [1, 3, 5])
    })
  })
 })
