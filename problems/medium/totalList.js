c45bb59772d0660ff7266e74a54ab538a0766919
Azeb.K <54040527+azebkidanemariam@users.noreply.github.com>
2021-11-03 10:10:34 +0100

    totallist part function added

--- problems/medium/totalList.js
+++ problems/medium/totalList.js
@@ -1,10 +1,19 @@
 
 // Write a function that computes the running total of a list.
-export const totalList = (xs) => {
+export const x = (xs) => {
 	// Your code here...
+	xs.reduce((accumulator,currentValue) => {
+		return accumulator + currentValue
+	}, 0)
 	return -1
 }
-
+function totalList(xs){
+	let sum = 0
+	for(let x of xs){
+		sum += +x
+	}
+	return sum
+}
 const tests = [
 	[totalList, [[1, 2, 3, 4, 5]], 15],
 	[totalList, [[1, 2, 3, 4, 5, 6]], 21],
